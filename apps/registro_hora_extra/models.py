ebebb4896b0a732a3dc501514ddeae0b2101501f
Breno de Barros <brenowbarros21@gmail.com>
2023-01-31 16:04:46 -0400

    Criando app RegistroHoraExtra

--- apps/registro_hora_extra/models.py
+++ apps/registro_hora_extra/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class RegistroHoraExtra(models.Model):
+    motivo = models.CharField('Motivo', max_length=100)
+
+    def __str__(self):
+        return self.motivo
