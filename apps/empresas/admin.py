2b23e2c6fad7797de8cda1036c8ffb6a9c0d4a87
Breno de Barros <brenowbarros21@gmail.com>
2023-01-31 15:39:29 -0400

    Criando app Funcionarios

--- apps/empresas/admin.py
+++ apps/empresas/admin.py
@@ -6,3 +6,7 @@ from .models import Empresa
 @admin.register(Empresa)
 class EmpresaAdmin(admin.ModelAdmin):
     list_display = ['nome']
+
+    class Meta:
+        verbose_name = 'Empresa'
+        verbose_name_plural = 'Empresas'
