55a39816ae2997684c180bb2df95f42d238910c9
Breno de Barros <brenowbarros21@gmail.com>
2023-01-31 22:37:25 -0400

    Implementando os relacionamentos entre models

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,8 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+from apps.departamentos.models import Departamento
+from apps.empresas.models import Empresa
 
 
 class Funcionario(models.Model):
     nome = models.CharField('Nome', max_length=100)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    departamentos = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nome
