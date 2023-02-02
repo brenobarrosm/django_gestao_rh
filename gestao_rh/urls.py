0d5b6337abf00beb9ab6efb76d4fa8f8e09d6afd
Breno de Barros <brenowbarros21@gmail.com>
2023-02-01 22:14:29 -0400

    CRUD Empresa

--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('apps.core.urls')),
-    path('funcionarios/', include('apps.funcionarios.urls'))
+    path('funcionarios/', include('apps.funcionarios.urls')),
+    path('empresa/', include('apps.empresas.urls')),
 ]
