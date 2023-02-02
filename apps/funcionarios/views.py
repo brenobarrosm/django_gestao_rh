646c9eaeb803d7740cd8adbcaf2f1f6a4fd49f21
Breno de Barros <brenowbarros21@gmail.com>
2023-02-02 11:10:08 -0400

    Adicionando Create Funcionario

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import DeleteView, ListView, UpdateView
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 
 from .models import Funcionario
 
@@ -20,3 +21,15 @@ class FuncionarioEdit(UpdateView):
 class FuncionarioDelete(DeleteView):
     model = Funcionario
     success_url = reverse_lazy('list_funcionarios')
+
+class FuncionarioCreate(CreateView):
+    model = Funcionario
+    fields = ['nome', 'departamentos']
+
+    def form_valid(self, form):
+        funcionario = form.save(commit=False)
+        username = funcionario.nome.split(' ')[0] + funcionario.nome.split(' ')[1]
+        funcionario.empresa = self.request.user.funcionario.empresa
+        funcionario.user = User.objects.create(username=username)
+        funcionario.save()
+        return super(FuncionarioCreate, self).form_valid(form)
