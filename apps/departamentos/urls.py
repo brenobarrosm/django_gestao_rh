1e3b992629f40af9fa6fcad9e57fb472dd70f81d
Breno de Barros <brenowbarros21@gmail.com>
2023-02-03 22:10:27 -0400

    Adicionando Delete Departamento

--- apps/departamentos/urls.py
+++ apps/departamentos/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import DepartamentoCreate, DepartamentosList, DepartamentoUpdate
+from .views import (DepartamentoCreate, DepartamentoDelete, DepartamentosList,
+                    DepartamentoUpdate)
 
 urlpatterns = [
     path('list/', DepartamentosList.as_view() , name='list_departamentos'),
     path('novo/', DepartamentoCreate.as_view(), name='create_departamento'),
     path('editar/<int:pk>', DepartamentoUpdate.as_view(), name='update_departamento'),
+    path('deletar/<int:pk>', DepartamentoDelete.as_view(), name='delete_departamento')
 ]
