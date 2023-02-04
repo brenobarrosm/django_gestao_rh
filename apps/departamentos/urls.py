d1846147619affe44050ca5d6f028632aab339da
Breno de Barros <brenowbarros21@gmail.com>
2023-02-03 21:41:12 -0400

    Adicionando Create e Update Departamento

--- apps/departamentos/urls.py
+++ apps/departamentos/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import DepartamentosList
+from .views import DepartamentoCreate, DepartamentosList, DepartamentoUpdate
 
 urlpatterns = [
-    path('list/', DepartamentosList.as_view() , name='list_departamentos')
+    path('list/', DepartamentosList.as_view() , name='list_departamentos'),
+    path('novo/', DepartamentoCreate.as_view(), name='create_departamento'),
+    path('editar/<int:pk>', DepartamentoUpdate.as_view(), name='update_departamento'),
 ]
